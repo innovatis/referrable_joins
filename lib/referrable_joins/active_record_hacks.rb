994fdd3381f72c21eb0f961eba2fd8cb8364c2a8
Burke Libbey <burke@burkelibbey.org>
2011-04-04 00:23:25 -0500

    added spawnmethods

--- lib/referrable_joins/active_record_hacks.rb
+++ lib/referrable_joins/active_record_hacks.rb
@@ -1,4 +1,72 @@
 module ActiveRecord
+  
+  module SpawnMethods
+    def merge(r)
+      merged_relation = clone
+      return merged_relation unless r
+      return to_a & r if r.is_a?(Array)
+
+      Relation::ASSOCIATION_METHODS.each do |method|
+        value = r.send(:"#{method}_values")
+
+        unless value.empty?
+          if method == :includes
+            merged_relation = merged_relation.includes(value)
+          else
+            merged_relation.send(:"#{method}_values=", value)
+          end
+        end
+      end
+
+      (Relation::MULTI_VALUE_METHODS - [:joins, :where, :order]).each do |method|
+        value = r.send(:"#{method}_values")
+        merged_relation.send(:"#{method}_values=", merged_relation.send(:"#{method}_values") + value) if value.present?
+      end
+
+      order_value = r.order_values
+      if order_value.present?
+        if r.reorder_flag
+          merged_relation.order_values = order_value
+        else
+          merged_relation.order_values = merged_relation.order_values + order_value
+        end
+      end
+      
+      # MONKEYPATCH Only change, right here.
+      merged_relation = merged_relation.outer_joins(r.outer_joins_values).inner_join(r.inner_joins_values)
+
+      merged_wheres = @where_values + r.where_values
+
+      unless @where_values.empty?
+        # Remove duplicates, last one wins.
+        seen = Hash.new { |h,table| h[table] = {} }
+        merged_wheres = merged_wheres.reverse.reject { |w|
+          nuke = false
+          if w.respond_to?(:operator) && w.operator == :==
+            name              = w.left.name
+            table             = w.left.relation.name
+            nuke              = seen[table][name]
+            seen[table][name] = true
+          end
+          nuke
+        }.reverse
+      end
+
+      merged_relation.where_values = merged_wheres
+
+      Relation::SINGLE_VALUE_METHODS.reject {|m| m == :lock}.each do |method|
+        value = r.send(:"#{method}_value")
+        merged_relation.send(:"#{method}_value=", value) unless value.nil?
+      end
+
+      merged_relation.lock_value = r.lock_value unless merged_relation.lock_value
+
+      # Apply scope extension modules
+      merged_relation.send :apply_modules, r.extensions
+
+      merged_relation
+    end
+  end 
 
   class ReflectionTable
    
@@ -31,7 +99,7 @@ module ActiveRecord
       delegate :outer_joins, :inner_joins, :to => :scoped
       
       def reflection_table(reflection)
-        ActiveRecord::ReflectionTable.new(self.reflections[reflection].table_name.to_sym)
+        ActiveRecord::ReflectionTable.new(self.reflections[reflection].table_name.to_sym, reflection)
       end 
       
     end 
--- lib/referrable_joins/version.rb
+++ lib/referrable_joins/version.rb
@@ -1,3 +1,3 @@
 module ReferrableJoins
-  VERSION = "0.2.0"
+  VERSION = "0.2.1"
 end
